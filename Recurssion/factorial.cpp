5116a95e80f7f120679b328ae12c15cec3d4c524
Alamin <mmia2330668@gmail.com>
2025-03-06 14:44:36 +0600

    n

--- Recurssion/factorial.cpp
+++ Recurssion/factorial.cpp
@@ -1,17 +1,20 @@
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-int factorial(int n){
-    if(n==1)return 1;
+int factorial(int n)
+{
+    if (n == 1)
+        return 1;
 
-    return n*factorial(n-1);
+    return n * factorial(n - 1);
 }
 
-int main() {
-    
+int main()
+{
+
     int n;
-    cin>>n;
-    cout<<factorial(n);
-    
+    cin >> n;
+    cout << factorial(n);
     return 0;
 }
